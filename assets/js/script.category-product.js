97b1237a4acbb219f7763ae0088e66b25e91931f
João Victor <joaosantos.senai564@gmail.com>
2023-08-18 14:14:39 -0300

    feat:  create updateCategory method

--- assets/js/script.category-product.js
+++ assets/js/script.category-product.js
@@ -37,6 +37,12 @@ class CategoryService {
     getCategoryById(id){
         return  this.categories.find((category) => category.id == id);
      }
+
+     //U => Update
+     upadateCategory(id, name) {
+        const category = this.getCategoryById(id);
+        category.name = name;
+     }
 }
 
 
